da9e30802b23397d39559c66e6aa4bf2a7d3dc35
kaktusbezcherepa <eli1starboom@gmail.com>
2024-05-13 00:08:46 +0300

    kek

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -2,7 +2,11 @@ import "./Search.css"
 import Man from "../../../assets/models/model-man.png"
 import Women from "../../../assets/models/model-women.png"
 import Man2 from '../../../assets/models/model-man-2.png'
-import Niggers from "../../../assets/models/model-man-niggers.png"
+import Man3 from "../../../assets/models/model-man-3.png"
+import Delivery from "../../../assets/icons/delivery.svg"
+import Return from "../../../assets/icons/return.svg"
+import Secure from "../../../assets/icons/secure.svg"
+import Tracking from "../../../assets/icons/tracking.svg"
 
 
 export const Search = () => {
@@ -22,8 +26,8 @@ export const Search = () => {
           </div>
       </div>
       <div className="btn-container">
-            <button>
-             Button
+            <button className="button-models catalog">
+             Catalog
             </button>
       </div>
 
@@ -34,13 +38,13 @@ export const Search = () => {
       <div className="models">
         <div className="image-container">
         <img src={Man} alt="Man" />
-        <button className="button-on-image">
+        <button className="button-on-image button-models">
          buy
         </button>
       </div>
   <div className="image-container">
     <img src={Women} alt="Women" />
-        <button className="button-on-image">
+        <button className="button-on-image button-models">
          Buy
         </button>
         </div>
@@ -56,17 +60,36 @@ export const Search = () => {
       <div className="models under-blog">
         <div className="image-container">
         <img src={Man2} alt="Man" />
-        <button className="button-on-image">
+        <button className="button-on-image button-models">
          Read
         </button>
       </div>
     <div className="image-container">
-    <img src={Niggers} alt="men-photo2" />
-        <button className="button-on-image">
+    <img src={Man3} alt="men-photo2" />
+        <button className="button-on-image button-models">
          Read
         </button>
         </div>
     </div>      
+
+    <div className="prefooter">
+  <div className="prefooter-text">
+        <p>Sign up to our newsletter for all <br /> the latest news & discounts.</p>
+        <div className="input-container">
+          <input placeholder="E-mail adress" className="input-under-footer-text" type="text" />
+          <button  className="arrow-button"></button>
+        </div>
+      </div>
+    </div> 
+
+    <div className="container-column">
+        <div className="column"><img src={Delivery} className="footer-icons" />FAST DELIVERY </div>
+        <div className="column"><img src={Return} className="return footer-icons"  />FREE RETURNS BLOCK</div>
+        <div className="column"><img src={Secure} className="footer-icons" />SECURE CHECKOUT</div>
+        <div className="column"><img src={Tracking} className="footer-icons" />ORDER TRACKING</div>
+    </div>
+
+
 
       <div className="zaglushka"></div>
     </>

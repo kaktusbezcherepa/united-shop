9ac43404dcaea775e2fc670e55b3a1bd8345f83a
Artem <artval570@gmail.com>
2024-05-15 21:22:45 +0300

    Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,37 +2,30 @@ import './Footer.css'
 
 function Footer() {
   return (
-    <>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <h5>UNITED</h5>
-            <ul>
-              <li><a href="#">SUPPORT</a></li>
-              <li><a href="#">Help & Contact Us</a></li>
-              <li><a href="#">Return & Refunds</a></li>
-              <li><a href="#">Privacy Policy</a></li>
-              <li><a href="#">FAQ</a></li>
-            </ul>
-          </div>
-          <div className="col-md-4">
-            <h5>COMPANY</h5>
-            <ul>
-              <li><a href="#">What we do</a></li>
-              <li><a href="#">Gift Offers</a></li>
-            </ul>
-          </div>
-          <div className="col-md-4">
-            <h5>CONTACTS</h5>
-            <ul>
-              <li>+7 (950) 777-77-77</li>
-              <li><a href="mailto:unitedclothes@mail.ru">unitedclothes@mail.ru</a></li>
-            </ul>
-            <p>&copy; 2024, ALL RIGHTS RESERVED UNITED</p>
-          </div>
-        </div>
-      </div>
-    </>
+    <footer>
+    <div class="container">
+      <div class="column1">
+      <h2>UNITED</h2>
+    </div>
+    <div class="column2">
+      <h3>SUPPORT</h3>
+      <p>Help & Contact us</p>
+      <p>Return & Refunds</p>
+      <p>Privacy Police</p>
+      <p>FAQ</p>
+    </div>
+    <div class="column3">
+      <h3>COMPANY</h3>
+      <p>What we do</p>
+      <p>Gift Offers</p>
+    </div>
+    <div class="column4">
+      <h3>CONTACTS</h3>
+      <p>+7 (950) 777-77-77</p>
+      <p>unitedclothes@mail.ru</p>
+    </div>
+  </div>
+</footer>
   );
 };
 

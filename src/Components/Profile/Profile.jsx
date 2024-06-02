20360dc538608888c7c694da70edc7b26234ec39
Artem <artval570@gmail.com>
2024-06-02 18:56:44 +0300

    reg

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,10 +1,35 @@
-
+import "./Profile.css"
 
 export const Profile = () => {
   return (
-    <h1>
-        profile
-    </h1>
-    
-  )
+    <div className="container">
+      <div className="form-container">
+        <h1>SIGN IN</h1>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+          />
+        </div>
+        <button onClick={(e) => {
+          e.preventDefault();
+          console.log('Email:', document.getElementById('email').value);
+          console.log('Password:', document.getElementById('password').value);
+        }}>SIGN IN</button>
+        <button className="register-button" onClick={() => {
+          // Регистрация
+        }}>REGISTER</button>
+      </div>
+    </div>
+  );
 }

8a1c4acbad2f70ace499e6a871483f47568b223b
ZakharBlinov <blinov.0121z@gmail.com>
2024-06-03 11:06:22 +0300

    preButton

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,8 +1,33 @@
 import "./Profile.css"
+import sadEmoji from "../../../assets/icons/sad-emoji.svg"
+import Footer from "../Footer/Footer"
+import Exit from "../../../assets/icons/exit.svg"
+import { useState } from "react"
+
 
 export const Profile = () => {
+  const [isOpenProfile, setIsOpenProfile] = useState(false);
+
+  const toggleModale = () => {
+    setIsOpenProfile(!isOpenProfile);
+  }
   return (
-    <div className="container">
+    
+    <>
+    <div className="test"></div>
+      <div className="profile-unreg-main-text">
+          <h1 className="not-availiable">This page is only available to authorized users</h1>
+     </div>
+     <div className="profile-unreg-icon">
+          <img src={sadEmoji}/>
+      </div>
+      <div className="unreg-button-container">
+        <button onClick={toggleModale} className="unreg-button">Log / Reg</button>
+        {isOpenProfile && (
+          <div className="exit-icon-profile-container">
+          <button onClick={toggleModale}><img className="exit-icon-profile"src={Exit}/></button>
+          <div>
+          <div className="container">
       <div className="form-container">
         <h1>SIGN IN</h1>
         <div>
@@ -27,8 +52,13 @@ export const Profile = () => {
           console.log('Password:', document.getElementById('password').value);
         }}>SIGN IN</button>
         <button className="register-button" onClick={() => {
-          // Регистрация
         }}>REGISTER</button>
       </div>
     </div>
+          </div>
+        </div>
+      )}
+      </div>
+      <Footer />
+    </>
 ) }

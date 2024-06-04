79e7ff3148d538006e7ba0d97eb81bbb7353aa06
Artem <artval570@gmail.com>
2024-06-04 23:47:38 +0300

    test

--- src/Components/test/test.jsx
+++ src/Components/test/test.jsx
@@ -1,11 +1,30 @@
+import Footer from '../Footer/Footer';
+import './test.css';
 
-
-const Test = () => {
+export const Test = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div className='global'>
+        <div className='profile-info-text'>PROFILE</div>
+        <div className='mydata'>MY DATA</div>
+        <table className='sidebar'>
+          <tr>
+            <th className='tho'>MY DATA</th>
+          </tr>
+          <tr>
+            <td className='tdo'>ORDERS</td>
+          </tr>
+          <tr>
+            <td className='tdo'>CLUB CARD</td>
+          </tr>
+        </table>
+        <label className='table-main' for="name"><b>Name</b></label><input className='input-main' type="text" name="name" required></input>
+        <label className='table-main' for="name"><b>Name</b></label><input className='input-main' type="text" name="name" required></input>
+        <label className='table-main' for="name"><b>Name</b></label><input className='input-main' type="text" name="name" required></input>
+        </div>
+      <Footer />
+    </>
+  );
+};
 
-export default Test
+export default Test;

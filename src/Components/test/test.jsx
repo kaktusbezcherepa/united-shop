e6c4508752aac102af4b50c72c0b588a8391bdd3
Artem <artval570@gmail.com>
2024-06-06 14:45:42 +0300

    profiletest

--- src/Components/test/test.jsx
+++ src/Components/test/test.jsx
@@ -1,4 +1,5 @@
 import './test.css';
+import { Link } from "react-router-dom"
 
 export const Test = () => {
   return (
@@ -7,9 +8,9 @@ export const Test = () => {
       <h1 className='profile-my-data'>profile</h1>
       <h2 className='my-data-text'>my data</h2>
       <div className="buttons-profile-container">
-        <div className="buttons-profile1"><p className='profile-swap-win-button'>my data</p></div>
-        <div className="buttons-profile2"><p className='profile-swap-win-button'>orders</p></div>
-        <div className="buttons-profile3"><p className='profile-swap-win-button'>club card</p></div>
+        <div className="buttons-profile1"><p className='custom-varrior-button'>my data</p></div>
+        <div className="buttons-profile3"><p className='profile-swap-win-button'><Link to={"/orders"}>orders</Link></p></div>
+        <div className="buttons-profile3"><p className='profile-swap-win-button'><Link to={"/clubcard"}>club card</Link></p></div>
       </div>
 
       <div className="testik">
@@ -24,7 +25,7 @@ export const Test = () => {
         </div>
         <div className="input-fields">
           <h3 className='field-name'>Date of birth</h3>
-          <input className='input-field' type="text" />
+          <input className='input-field' type="data" name="trip-start" value="2018-07-22" min="2018-01-01" max="2018-12-31" />
         </div>
         <div className="input-fields">
           <h3 className='field-name'>Email</h3>
@@ -45,8 +46,6 @@ export const Test = () => {
       </div>
       
       </div>
-
-
 
 
       <div className="zagla"></div>

49a4dd7801c0d7327ef48bc9149556ef1f1f7c7e
Artem <artval570@gmail.com>
2024-06-13 21:42:41 +0300

    addphotos

---
+++ src/WomenLimitedData.js
@@ -0,0 +1,12 @@
+export const WomenLimited = [
+    { id: 1, name: 'T-short', price: 322, image: '/assets/items/WomenLimited/blouse1.webp', brand: "Rick Owens" },
+    { id: 2, name: 'T-short', price: 300, image: '/assets/items/WomenLimited/bodysuit1.webp', brand: "Number 9" },
+    { id: 3, name: 'T-short', price: 200, image: '/assets/items/WomenLimited/boxy1.webp', brand: "True Religion" },
+    { id: 4, name: 'T-short', price: 150, image: '/assets/items/WomenLimited/jeans1.webp', brand: "Rick Owens" },
+    { id: 5, name: 'T-short', price: 100, image: '/assets/items/WomenLimited/jeans2.webp', brand: "Number 9" },
+    { id: 6, name: 'T-short', price: 555, image: '/assets/items/WomenLimited/jeans3.webp', brand: "Rick Owens" },
+    { id: 7, name: 'T-short', price: 231, image: '/assets/items/WomenLimited/jeans4.webp', brand: "True Religion" },
+    { id: 8, name: 'T-short', price: 200, image: '/assets/items/WomenLimited/jeans5.webp', brand: "True Religion" },
+    { id: 9, name: 'T-short', price: 200, image: '/assets/items/WomenLimited/jeans6.webp', brand: "True Religion" },
+    { id: 10, name: 'T-short', price: 200, image: '/assets/items/WomenLimited/jeans7.webp', brand: "True Religion" }
+];

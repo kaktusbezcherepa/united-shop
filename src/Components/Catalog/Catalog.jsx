892f99b7da1dc406b1c46e2b31c4f8624db5bc17
Artem <artval570@gmail.com>
2024-06-17 10:00:30 +0300

    fixmain

--- src/Components/Catalog/Catalog.jsx
+++ src/Components/Catalog/Catalog.jsx
@@ -148,7 +148,7 @@ export const Catalog = () => {
   return (
     <>
     <div className="test"></div>
-      <div className="catalog-main-img">
+      <div className="summer-collection-men-men">
           <div className='catalog-main-img-text'>Sale</div>
           <div className='catalog-main-img-text2'>up to</div>
           <div className='catalog-main-img-text3'>60%</div>
